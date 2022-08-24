2dcfe3aa9028d4d4345ac6892383f5a6a1523f70
BrianGaleanoSion <83656123+BrianGaleanoSion@users.noreply.github.com>
2022-08-24 00:08:16 -0600

    Didi disconect IOS

--- ios/Classes/FlutterBluePlugin.m
+++ ios/Classes/FlutterBluePlugin.m
@@ -35,6 +35,7 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
 @property(nonatomic, retain) FlutterBlueStreamHandler *stateStreamHandler;
 @property(nonatomic, retain) CBCentralManager *centralManager;
 @property(nonatomic) NSMutableDictionary *scannedPeripherals;
+@property(nonatomic) NSMutableDictionary *connectedPeripherals;
 @property(nonatomic) NSMutableArray *servicesThatNeedDiscovered;
 @property(nonatomic) NSMutableArray *characteristicsThatNeedDiscovered;
 @property(nonatomic) LogLevel logLevel;
@@ -50,6 +51,7 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
   instance.channel = channel;
   instance.centralManager = [[CBCentralManager alloc] initWithDelegate:instance queue:nil];
   instance.scannedPeripherals = [NSMutableDictionary new];
+  instance.connectedPeripherals = [NSMutableDictionary new];
   instance.servicesThatNeedDiscovered = [NSMutableArray new];
   instance.characteristicsThatNeedDiscovered = [NSMutableArray new];
   instance.logLevel = emergency;
@@ -106,6 +108,10 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
   } else if([@"getConnectedDevices" isEqualToString:call.method]) {
     // Cannot pass blank UUID list for security reasons. Assume all devices have the Generic Access service 0x1800
     NSArray *periphs = [self->_centralManager retrieveConnectedPeripheralsWithServices:@[[CBUUID UUIDWithString:@"1800"]]];
+    // Insert connecteds devices in a NSMutableDictionary set peripheral and UUID
+      for(CBPeripheral *p in periphs) {
+          [self.connectedPeripherals setObject:p forKey:[[p identifier] UUIDString]];
+      }
     NSLog(@"getConnectedDevices periphs size: %lu", [periphs count]);
     result([self toFlutterData:[self toConnectedDeviceResponseProto:periphs]]);
   } else if([@"connect" isEqualToString:call.method]) {
@@ -113,14 +119,21 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
     ProtosConnectRequest *request = [[ProtosConnectRequest alloc] initWithData:[data data] error:nil];
     NSString *remoteId = [request remoteId];
     @try {
+      CBPeripheral *peripheralConnected = [_connectedPeripherals objectForKey:remoteId];
       CBPeripheral *peripheral = [_scannedPeripherals objectForKey:remoteId];
-      if(peripheral == nil) {
-        @throw [FlutterError errorWithCode:@"connect"
+        if(peripheralConnected == nil) {
+          if(peripheral == nil) {
+          @throw [FlutterError errorWithCode:@"connect"
                                    message:@"Peripheral not found"
                                    details:nil];
       }
-      // TODO: Implement Connect options (#36)
-      [_centralManager connectPeripheral:peripheral options:nil];
+       // TODO: Implement Connect options (#36)
+        if([peripheral state] == CBPeripheralStateDisconnected || [peripheral state] == CBPeripheralStateDisconnecting){
+          [_centralManager connectPeripheral:peripheral options:nil];
+        }
+      } else {
+          [_centralManager connectPeripheral:peripheralConnected options:nil];
+      }
       result(nil);
     } @catch(FlutterError *e) {
       result(e);
@@ -253,6 +266,15 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
     }
   } else if([@"requestMtu" isEqualToString:call.method]) {
     result([FlutterError errorWithCode:@"requestMtu" message:@"iOS does not allow mtu requests to the peripheral" details:NULL]);
+  } else if([@"readRssi" isEqualToString:call.method]) {
+    NSString *remoteId = [call arguments];
+    @try {
+      CBPeripheral *peripheral = [self findPeripheral:remoteId];
+      [peripheral readRSSI];
+      result(nil);
+    } @catch(FlutterError *e) {
+      result(e);
+    }
   } else {
     result(FlutterMethodNotImplemented);
   }
@@ -377,7 +399,8 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
 - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
   NSLog(@"didConnectPeripheral");
   // Register self as delegate for peripheral
-  peripheral.delegate = self;
+  // se o usuáro disconnectar manualmente o código do erro será 0
+ peripheral.delegate = self;
   
   // Send initial mtu size
   uint32_t mtu = [self getMtu:peripheral];
@@ -390,7 +413,19 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
 - (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error {
   NSLog(@"didDisconnectPeripheral");
   // Unregister self as delegate for peripheral, not working #42
-  peripheral.delegate = nil;
+    // if the user disconnect manual the code error is 0
+   if([error code] != 0) {
+     // trying reconnect
+    @try {
+      NSLog(@"didConnectPeripheral:trying reconnect");
+      [central connectPeripheral: peripheral options:nil];
+    } @catch(FlutterError * e) {
+      NSLog(@"didConnectPeripheral:can't reconnect");
+    } 
+  } else {
+    NSLog(@"didDisconnectPeripheral:user disconnected");
+  peripheral.delegate = self;
+  }
   
   // Send connection state
   [_channel invokeMethod:@"DeviceState" arguments:[self toFlutterData:[self toDeviceStateProto:peripheral state:peripheral.state]]];
@@ -398,6 +433,17 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
 
 - (void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error {
   // TODO:?
+  // TODO:? Just going to try to issue a reconnect
+  NSLog(@"didFailToReconnectPeripheral");
+
+  //Conexão falhou -> tenta conectar novamente
+  @try {
+    NSLog(@"didFailToReconnectPeripheral:tentando conectar");
+    [central connectPeripheral:peripheral options:nil];
+  } @catch (FlutterError *e){
+    NSLog(@"didFailToReconnectPeripheral: conexão falhou");
+  }
+  [_channel invokeMethod:@"DeviceState" arguments:[self toFlutterData:[self toDeviceStateProto:peripheral state: peripheral.state]]];
 }
 
 //
@@ -536,6 +582,13 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
   [result setRequest:request];
   [result setSuccess:(error == nil)];
   [_channel invokeMethod:@"WriteDescriptorResponse" arguments:[self toFlutterData:result]];
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didReadRSSI:(NSNumber *)rssi error:(NSError *)error {
+  ProtosReadRssiResult *result = [[ProtosReadRssiResult alloc] init];
+  [result setRemoteId:[peripheral.identifier UUIDString]];
+  [result setRssi:[rssi intValue]];
+  [_channel invokeMethod:@"ReadRssiResult" arguments:[self toFlutterData:result]];
 }
 
 //
